4eb0084f03a8b385902a93efad5d71349b9e85c6
Keshav Malhotra <malhotrakeshav10@gmail.com>
2025-05-31 20:51:10 -0400

    commented out unused import

--- RobotBuild-Tank/src/main/java/frc/robot/Robot.java
+++ RobotBuild-Tank/src/main/java/frc/robot/Robot.java
@@ -12,7 +12,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import com.revrobotics.spark.SparkBase;
 import com.revrobotics.spark.SparkLowLevel;
 import com.revrobotics.spark.SparkMax;
-import com.revrobotics.spark.config.SparkBaseConfig;
+//import com.revrobotics.spark.config.SparkBaseConfig;
 import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 import com.revrobotics.spark.config.SparkMaxConfig;
 
